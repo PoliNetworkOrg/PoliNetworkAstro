3abf99462eeaa611580632cc2ba2b534ac421b24
Elia Maggioni <elia.maggioni@gmail.com>
2022-09-11 01:06:29 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: { 
-        assetsInclude: ['**/*.yml']
-    }
+    site: 'https://polinetworkorg.github.io',
+    base: '/PoliNetworkAstro',
 })
